a8caf43f156c1368e4b41ba871554ba55f5db787
tranthihangng <93020915+tranthihangng@users.noreply.github.com>
2023-04-11 23:28:22 +0700

    Create hanbg.py

---
+++ hanbg.py
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Điều khiển thiết bị</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		.b{width: 100px;height: 40px;font-size: 21px;color: #FFF;background-color:#4caf50;border-radius: 10px;}
+		.t{width: 100px;height: 40px;font-size: 21px;color: #FFF;background-color:#f44336;border-radius: 10px;}
+	  
+	  
+	  
+	  
+	  
+	  
+	</style>
+  </style>
+</head>
+<body>
+  <h2> 
+
+
+
+
+
+
+
+
+
+
+
+I(x,y)=D(p( X,Y)||P(X).P(Y)= TỔNG XÍCH MA CỦA CẬN DƯỚI ( X,Y)  P(X,Y)* LOG P(X,Y)/(P(X).P(Y)      
+	  
+	  
+	  
+	
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	 Giám sát và điều khiển vườn thông Minh BẰNG CÔNG NGHỆ  IOT  </h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Nhiệt độ</span> 
+    <span id="temperature">.............%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Độ ẩm vườn 1</span>
+    <span id="humidity">.............%</span>
+    <sup class="units">%</sup>
+  </p>
+	<p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Độ ẩm vườn 2</span>
+    <span id="humidity">.............%</span>
+    <sup class="units">%</sup>
+  </p>
+	
+	
+	
+	<p>
+	
+	<i class="fa-solid fa-cloud-sun" style="color:#00add6;"></i>
+	  <span class="dht-labels">Ánh sáng</span>
+    <span id="humidity">.............%</span>
+    <sup class="units">%</sup>
+  </p>
+	
+	
+	
+	
+	
+	<div style="width: 330px;height: auto;margin: 0 auto;margin-top: 70px">
+<h1 align="center">Điều Khiển </h1>
+	<table align="center">
+		<tr>
+			<td><a href='/bat1'><button class='b'>mở rèm</button></a><td>
+			<td><a href='/tat1'><button class='t'>kéo rèm </button></a><td>
+	    <tr>
+	    <tr>
+			<td><a href='/bat2'><button class='b'>Bật bơm</button></a><td>
+			<td><a href='/tat2'><button class='t'>Tắt bơm</button></a><td>
+	    <tr>
+	    <tr>
+			<td><a href='/bat3'><button class='b'>Bật đèn</button></a><td>
+			<td><a href='/tat3'><button class='t'>Tắt đèn</button></a><td>
+	    <tr>
+	    <tr>
+			<td><a href='/bat4'><button class='b'>Bật quạt </button></a><td>
+			<td><a href='/tat4'><button class='t'>Tắt quạt</button></a><td>
+      <tr>
+	    <tr>
+			<td><a href='/bat5'><button class='b'>Bật nóng lạnh </button></a><td>
+			<td><a href='/tat5'><button class='t'>Tắt nóng lạnh</button></a><td>     
+      <tr>
+	    <tr>
+			<td><a href='/bat6'><button class='b'>Mở cổng </button></a><td>
+			<td><a href='/tat6'><button class='t'>Đóng cổng</button></a><td>                                                                            
+	    <tr>	
+	</table>
+
+</body>
+<script>
